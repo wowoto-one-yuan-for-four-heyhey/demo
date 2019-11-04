aea4e450892cbae2e36586c626e54b7dbf8a70d6
11452 <1145222905@qq.com>
2019-11-04 20:10:39 +0800

    commit zach

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,6 +1,9 @@
 package com.example.demo.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+
+    @GetMapping("")
+    @ApiOperation("获得分类详情")
+    public Object cat_get(){return new Object();}
+
 }
